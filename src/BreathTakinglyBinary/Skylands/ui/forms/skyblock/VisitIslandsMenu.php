15450a7f84b50741b6996c3ed11e75512815fe29
95CivicSi <aaron_salyer@yahoo.com>
2019-10-25 01:35:03 -0400

    Properly build and handle Visit Islands menu.

--- src/BreathTakinglyBinary/Skylands/ui/forms/skyblock/VisitIslandsMenu.php
+++ src/BreathTakinglyBinary/Skylands/ui/forms/skyblock/VisitIslandsMenu.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 namespace BreathTakinglyBinary\Skylands\ui\forms\skyblock;
 
 
+use BreathTakinglyBinary\Skylands\isle\Isle;
 use BreathTakinglyBinary\Skylands\ui\forms\SimpleForm;
 use pocketmine\Player;
 use BreathTakinglyBinary\Skylands\Skylands;
 
 class VisitIslandsMenu extends SimpleForm{
 
+    /** @var Isle[] */
+    private $isles = [];
+
     public function __construct(?Player $player = null){
         parent::__construct();
         $skyBlock = Skylands::getInstance();
-        $islands = 0;
         $playerIsleId = null;
         if($player instanceof Player){
             $playerIsleId = $skyBlock->getSessionManager()->getSession($player)->getIsle()->getIdentifier();
@@ -21,17 +24,15 @@ class VisitIslandsMenu extends SimpleForm{
         foreach($skyBlock->getIsleManager()->getIsles() as $isle){
             if(!$isle->isLocked() and $isle->getIdentifier() !== $playerIsleId){
                 $this->addButton($isle->getIdentifier(), $isle->getIdentifier());
-                $islands++;
+                $this->isles[$isle->getIdentifier()] = $isle;
             }
         }
         $this->addButton("Back");
     }
 
     protected function onSubmit(Player $player, $data){
-        $isle = Skylands::getInstance()->getIsleManager()->getIsle($data);
-        if($isle !== null){
-            $player->teleport($isle->getSpawnLocation());
-
+        if(isset($this->isles[$data])){
+            $player->teleport($this->isles[$data]->getSpawnLocation());
             return;
         }
         $msg = $data === "Back" ? null : "Island $data, is not available.";
